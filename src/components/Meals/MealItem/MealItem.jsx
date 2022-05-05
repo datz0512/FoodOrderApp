4b57eaef32a78aff925815766155e9bd73acf0b9
Do Tien Dat <datblu2003@gmail.com>
2022-05-05 14:11:21 +0700

    adding a form

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,7 +1,8 @@
-import classes from './MealItem.module.scss'
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.scss";
 
 const MealItem = (props) => {
-    const price = `$${props.price.toFixed(2)}`
+  const price = `$${props.price.toFixed(2)}`;
 
   return (
     <li className={classes.meal}>
@@ -11,7 +12,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-
+        <MealItemForm id={props.id}/>
       </div>
     </li>
   );
